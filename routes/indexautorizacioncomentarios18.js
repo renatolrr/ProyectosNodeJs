dc78eeddd664ad77e4c1ad9eb7ffb3371b188939
root <root@localhost.localdomain>
2015-08-02 13:44:26 +0200

    Moderación de comentarios

--- routes/indexautorizacioncomentarios18.js
+++ routes/indexautorizacioncomentarios18.js
@@ -41,6 +41,8 @@ router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 //Rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',    commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
 
 //Autor 
 router.get('/author', function(req, res) {

d5e36ac09a6f9dcd8630646c1e3ed06c67094c5a
renatolrr <renatolrr@gmail.com>
2015-08-05 20:17:08 +0200

    Creando routes/index.js y user_controller.js por old1

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,41 @@
 var express = require('express');
-var multer  = require('multer');
 var router = express.Router();
-
-var quizController = require('../controllers/quiz_controller');
-var commentController = require('../controllers/comment_controller');
-var sessionController = require('../controllers/session_controller');
-var userController = require('../controllers/user_controller');
-
-// Página de entrada (home page)
+//console.log("Llega aqui 1");
+var quizController= require('../controllers/quiz_controller');
+var commentController= require('../controllers/comment_controller');
+var sessionController= require('../controllers/session_controller');
+/* GET home page. */
+//console.log("Llega aqui 7");
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors: []});
+res.render('index', { title: 'Quiz', errors: [] });
 });
-
-// Autoload de comandos con ids
-router.param('quizId', quizController.load);  // autoload :quizId
-router.param('commentId', commentController.load);  // autoload :commentId
-router.param('userId', userController.load);  // autoload :userId
-
-// Definición de rutas de sesion
-router.get('/login',  sessionController.new);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
-
-// Definición de rutas de cuenta
-router.get('/user',  userController.new);     // formulario sign un
-router.post('/user',  userController.create);     // registrar usuario
-router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.ownershipRequired, userController.edit);     // editar información de cuenta
-router.put('/user/:userId(\\d+)',  sessionController.loginRequired, userController.ownershipRequired, userController.update);     // actualizar información de cuenta
-router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, userController.ownershipRequired, userController.destroy);     // borrar cuenta
-router.get('/user/:userId(\\d+)/quizes',  quizController.index);     // ver las preguntas de un usuario
-
+//Autoload de comando con quizId
+router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload :quizId
+//Definición de reutas de sesion
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
-router.post('/quizes/create',              sessionController.loginRequired, multer({ dest: './public/media/'}), quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.ownershipRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.ownershipRequired, multer({ dest: './public/media/'}), quizController.update);
-router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.ownershipRequired, quizController.destroy);
-
-// Definición de rutas de comentarios
+// Requieren autorización
+router.get('/quizes/new' ,sessionController.loginRequired,quizController.new);
+router.post('/quizes/create' ,sessionController.loginRequired,quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit' ,sessionController.loginRequired,quizController.edit);
+router.put('/quizes/:quizId(\\d+)' ,sessionController.loginRequired,quizController.update);
+router.delete('/quizes/:quizId(\\d+)' ,sessionController.loginRequired,quizController.destroy);
+// Definición de rutas de los comentarios de /quizes
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
-router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
-	                                    sessionController.loginRequired, commentController.ownershipRequired, commentController.publish);
-
-
-//Autor  Ojo revisar
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+sessionController.loginRequired, commentController.publish);
 /* GET author page. */
 router.get('/author', function(req, res) {
   res.render('author', { autor: 'renatolrr', errors: [] });
 });
+
+
 
 module.exports = router;
